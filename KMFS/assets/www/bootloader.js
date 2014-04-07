4d79e9cb5df95a18f4051d2686e24652c800247f
Werner Venter <werner@clearcell.co.za>
2014-04-07 12:25:41 +0200

    Camera Foreground

--- KMFS/assets/www/bootloader.js
+++ KMFS/assets/www/bootloader.js
@@ -52,8 +52,8 @@ var destinationType; // sets the format of returned value
 
 // List of hosts where AtajoNode instance is running
 var bootHosts = [
-	 'http://192.168.43.23:5001',  // JACKIE LOCAL DEV
-	// 'http://192.168.10.122:4000', //JACKIE CENTURION
+	// 'http://192.168.43.23:5001',  // JACKIE LOCAL DEV
+	 'http://172.20.10.3:5001', //JACKIE CENTURION
 	// 'http://192.168.1.151:4000',  // WERNER LOCAL
    // 'http://192.168.4.99:4000', 
    //  'http://196.14.91.61:4000',  // zacptdev.brandhouse.co.za 
